740ea1b9fb2d4687cfd8399f9e9eff8de6983cc0
Alon Ben Haim <alon.b.h.672@gmail.com>
2020-03-23 14:49:53 +0200

    feat(user-control-activation): added activation/deactivation to user controls

---
+++ LearningConductors/ViewModels/ShellViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Caliburn.Micro;
+
+namespace LearningConductors.ViewModels
+{
+    public class ShellViewModel : Conductor<object>.Collection.AllActive
+    {
+        public RedViewModel Red { get; set; }
+        public GreenViewModel Green{ get; set; }
+        public BlueViewModel Blue { get; set; }
+        public ShellViewModel()
+        {
+            Red = new RedViewModel();
+            Green = new GreenViewModel();
+            Blue = new BlueViewModel();
+        }
+
+        private void ToggleActivation(Screen screen)
+        {
+            if (screen.IsActive)
+            {
+                DeactivateItem(screen, true);
+            }
+            else
+            {
+                ActivateItem(screen);
+            }
+        }
+
+        public void ToggleRed()
+        {
+            ToggleActivation(Red);
+        }
+
+        public void ToggleBlue()
+        {
+            ToggleActivation(Blue);
+        }
+
+        public void ToggleGreen()
+        {
+            ToggleActivation(Green);
+        }
+    }
+}
